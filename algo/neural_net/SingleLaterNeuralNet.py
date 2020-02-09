519833b8d4070226d517b4253dcfea74eef4a564
Karan Patel <karanpatel4567@gmail.com>
2020-02-08 17:01:47 -0800

    Adding training logic for a single epoch for single layer neural net

--- algo/neural_net/SingleLaterNeuralNet.py
+++ algo/neural_net/SingleLaterNeuralNet.py
@@ -14,36 +14,69 @@ class SingleLaterNeuralNet:
 
         self.__input_to_hidden_weights = SingleLaterNeuralNet.initialize_weight_matrix(
             shape=(num_hidden_units, training_samples[0].inputs.size))  # 50 * 785 . 785 * 1 = 50 * 1
+
         self.__hidden_to_output_weights = SingleLaterNeuralNet.initialize_weight_matrix(
             shape=(num_target_labels, num_hidden_units + 1))  # 10 * 51 *
 
-        self.__input_to_hidden_weights_delta_from_previous_itr = np.zeros(shape=self.__input_to_hidden_weights.shape,
-                                                                          dtype=np.double)
-        self.__hidden_to_output_weights_delta_from_previous_itr = np.zeros(shape=self.__hidden_to_output_weights.shape,
-                                                                           dtype=np.double)
+        self.__input_to_hidden_weights_delta_from_previous_itr = self.get_zero_matrix(self.__input_to_hidden_weights.shape)
+        self.__hidden_to_output_weights_delta_from_previous_itr = self.get_zero_matrix(self.__hidden_to_output_weights.shape)
 
     def train(self, learning_rate=0.1, momentum=0.1):
         for sample in self.__training_samples:
-            hidden_activations = list(map(SingleLaterNeuralNet.sigmoid_activation_function,
-                                          np.dot(self.__input_to_hidden_weights, sample.inputs)))
-            assert len(hidden_activations) == self.__num_hidden_units, 'Unexpected hidden activation array size'
 
-            output_activations = list(map(SingleLaterNeuralNet.sigmoid_activation_function,
-                                          np.dot(self.__hidden_to_output_weights, [1.0] + hidden_activations)))
-            assert len(output_activations) == self.__num_target_labels, 'Unexpected output activation array size'
+            hidden_activations = self.get_hidden_activations(sample)
+            output_activations = self.get_output_activations(hidden_activations)
 
-            output_error_terms = [
-                activation * (1 - activation) * (0.9 if act_num == sample.true_class_label else 0.1 - activation) for
-                act_num, activation in enumerate(output_activations)]
+            output_error_terms = self.get_output_error_terms(output_activations, sample)
             hidden_error_terms = self.get_hidden_error_terms(hidden_activations, output_error_terms)
 
-            hidden_to_output_delta = np.zeros(shape=self.__hidden_to_output_weights.shape, dtype=np.double)
-            # Δwk,j=ηδkhj+αΔwʹk,jwhereΔwʹk,jis change to this weight from previous iteration
-
+            hidden_to_output_delta = self.calculate_hidden_to_output_delta(hidden_activations, learning_rate, momentum, output_error_terms)
             self.__hidden_to_output_weights = self.__hidden_to_output_weights + hidden_to_output_delta
 
-            input_to_hidden_delta = np.zeros(shape=self.__input_to_hidden_weights.shape, dtype=np.double)
+            input_to_hidden_delta = self.calculate_input_to_hidden_delta(hidden_error_terms, learning_rate, momentum, sample)
             self.__input_to_hidden_weights = self.__input_to_hidden_weights + input_to_hidden_delta
+
+            self.__hidden_to_output_weights_delta_from_previous_itr = hidden_to_output_delta
+            self.__input_to_hidden_weights_delta_from_previous_itr = input_to_hidden_delta
+
+    def calculate_input_to_hidden_delta(self, hidden_error_terms, learning_rate, momentum, sample):
+        input_to_hidden_delta = []
+        for i_num, i in enumerate(hidden_error_terms):
+            delta = []
+            for j_num, j in enumerate(sample.inputs):
+                delta.append((learning_rate * i * j) + (
+                            momentum * self.__input_to_hidden_weights_delta_from_previous_itr[i_num, j_num]))
+            input_to_hidden_delta.append(delta)
+        return np.array(input_to_hidden_delta, dtype=np.double)
+
+    def calculate_hidden_to_output_delta(self, hidden_activations, learning_rate, momentum, output_error_terms):
+        hidden_to_output_delta = []
+        for i_num, i in enumerate(output_error_terms):
+            delta = []
+            for j_num, j in enumerate([1.0] + hidden_activations):
+                delta.append((learning_rate * i * j) + (
+                            momentum * self.__hidden_to_output_weights_delta_from_previous_itr[i_num, j_num]))
+            hidden_to_output_delta.append(delta)
+        return np.array(hidden_to_output_delta, dtype=np.double)
+
+    @staticmethod
+    def get_output_error_terms(output_activations, sample):
+        output_error_terms = [
+            activation * (1 - activation) * (0.9 if act_num == sample.true_class_label else 0.1 - activation) for
+            act_num, activation in enumerate(output_activations)]
+        return output_error_terms
+
+    def get_output_activations(self, hidden_activations):
+        output_activations = list(map(SingleLaterNeuralNet.sigmoid_activation_function,
+                                      np.dot(self.__hidden_to_output_weights, [1.0] + hidden_activations)))
+        assert len(output_activations) == self.__num_target_labels, 'Unexpected output activation array size'
+        return output_activations
+
+    def get_hidden_activations(self, sample):
+        hidden_activations = list(map(SingleLaterNeuralNet.sigmoid_activation_function,
+                                      np.dot(self.__input_to_hidden_weights, sample.inputs)))
+        assert len(hidden_activations) == self.__num_hidden_units, 'Unexpected hidden activation array size'
+        return hidden_activations
 
     def get_hidden_error_terms(self, hidden_activations, output_error_terms):
         hidden_error_terms = []
@@ -61,3 +94,7 @@ class SingleLaterNeuralNet:
     def initialize_weight_matrix(shape):
         weights = [[random.uniform(-0.5, 0.5) for i in range(shape[1])] for _ in range(shape[0])]
         return np.array(weights, dtype=np.double)
+
+    @staticmethod
+    def get_zero_matrix(shape):
+        return np.zeros(shape=shape, dtype=np.double)
