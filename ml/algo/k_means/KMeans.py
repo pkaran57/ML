093a7fe80a2efd0c9ee01efb7fc4ec043c4b8791
Karan Patel <karanpatel4567@gmail.com>
2020-03-02 10:28:27 -0800

    Placing samples into clusters and updating centroids for the k-means algo

--- ml/algo/k_means/KMeans.py
+++ ml/algo/k_means/KMeans.py
@@ -1,3 +1,5 @@
+from itertools import starmap
+
 import numpy as np
 
 
@@ -9,9 +11,25 @@ class KMeans:
         self._training_samples = training_samples
         self._validation_samples = validation_samples
 
-    def find_clusters(self, num_clusters):
+    def find_clusters(self, num_clusters, times_itr):
         centroids = [np.random.rand(self._training_samples[0].features.shape[0]) for _ in range(num_clusters)]
+        previous_centroid = [centroid for centroid in centroids]
 
-        for sample in self._training_samples:
-            distances = [KMeans.find_euclidean_distance(sample.features, centroids[cluster_num]) for cluster_num in range(num_clusters)]
-            print(distances)
+        for _ in range(times_itr):
+
+            groups = {cluster_num: list() for cluster_num in range(num_clusters)}
+
+            for sample in self._training_samples:
+                distances = [KMeans.find_euclidean_distance(sample.features, centroids[cluster_num]) for cluster_num in range(num_clusters)]
+                closest_to_centroid = distances.index(min(distances))
+                groups[closest_to_centroid].append(sample)
+
+            previous_centroid = [centroid for centroid in centroids]
+
+            for cluster_num, samples in groups.items():
+                if samples:
+                    np_array = np.array([sample.features for sample in samples])
+                    centroids[cluster_num] = np_array.sum(axis=0) / len(samples)
+
+            print('Difference in centroids from previous iteration = ',
+                  list(starmap(lambda centroid, previous_centroid: KMeans.find_euclidean_distance(centroid, previous_centroid), zip(centroids, previous_centroid))))
